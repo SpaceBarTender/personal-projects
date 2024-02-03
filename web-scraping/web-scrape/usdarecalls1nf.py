e208ac3c422f4c4999f92e46debbf857af90f468
SpaceBarTender <adumas97@gmail.com>
2024-02-03 07:45:30 -0600

    View website html to survey necessary parts

--- web-scraping/web-scrape/usdarecalls1nf.py
+++ web-scraping/web-scrape/usdarecalls1nf.py
@@ -7,3 +7,14 @@ import itertools
 import re
 import lxml
 from urllib.request import urlopen
+
+def viewUSDA():
+    page=0
+    url = f"https://www.fsis.usda.gov/recalls?page={page}"
+    response = requests.get(url)
+    html = response.content
+    soup = bs(html, "lxml")
+    view_content = soup.find('div', class_='view__content')
+    return view_content
+
+print(viewUSDA())
