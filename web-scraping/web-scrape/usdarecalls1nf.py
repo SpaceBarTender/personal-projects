c796df7e319a193853f47f1f793b24f26f357b7a
SpaceBarTender <adumas97@gmail.com>
2024-02-03 07:40:33 -0600

    Creat pipenv and pipfile with dependencies. Update pipfile with requests and lxml

---
+++ web-scraping/web-scrape/usdarecalls1nf.py
@@ -0,0 +1,9 @@
+import requests
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+from bs4 import BeautifulSoup as bs
+import itertools
+import re
+import lxml
+from urllib.request import urlopen
