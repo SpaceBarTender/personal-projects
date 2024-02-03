704cbe2274565757a26012ce0d4f51b87d7e1eb3
SpaceBarTender <adumas97@gmail.com>
2024-02-03 08:02:56 -0600

    Put body of scrape in while loop for multiple pages, add links list to actually take out link

--- web-scraping/web-scrape/usdarecalls1nf.py
+++ web-scraping/web-scrape/usdarecalls1nf.py
@@ -19,32 +19,39 @@ def viewUSDA():
     link_ = []
 
     page=0
-    url = f"https://www.fsis.usda.gov/recalls?page={page}"
-    response = requests.get(url)
-    html = response.content
-    soup = bs(html, "lxml")
-    view_content = soup.find('div', class_='view__content')
-    view_rows = view_content.find_all('div', class_='view__row')
-    for row in view_rows:
-            status = row.find('div', class_="recall-teaser__status").text.replace('\n',' ')
-            location = row.find('div', class_="recall-teaser__states")
-            if "Active" in status and location is not None:
-                view_status = row.find('span', class_="tag tag--active").text
-                status_.append(status)
-                teaser_title = row.find('h3', class_='recall-teaser__title').text
-                title_.append(teaser_title)
-                reason = row.find('a', class_="tag tag--reason").text
-                reason_.append(reason)
-                location2 = row.find('div', class_="recall-teaser__states").text
-                location_.append(location2)
-                date = row.find('div', class_="recall-teaser__date").text.replace('\n','')
-                date_.append(date)
-                summary = row.find('div', class_="recall-teaser__summary").text.replace('\n','')
-                summary_.append(summary)
-                impacted_products = row.find('div', class_="recall-teaser__products").text
-                impacted_products_.append(impacted_products)
-                link = row.find("a", href=re.compile("^(/recalls-alerts/)((?!:).)*$"))
-                link_.append(link)
-    return view_status, location2, teaser_title, reason, date, summary, impacted_products, link
+    while page != 4:
+        url = f"https://www.fsis.usda.gov/recalls?page={page}"
+        response = requests.get(url)
+        html = response.content
+        soup = bs(html, "lxml")
+        view_content = soup.find('div', class_='view__content')
+        view_rows = view_content.find_all('div', class_='view__row')
+        for row in view_rows:
+                status = row.find('div', class_="recall-teaser__status").text.replace('\n',' ')
+                location = row.find('div', class_="recall-teaser__states")
+                if "Active" in status and location is not None:
+                    view_status = row.find('span', class_="tag tag--active").text
+                    status_.append(status)
+                    teaser_title = row.find('h3', class_='recall-teaser__title').text
+                    title_.append(teaser_title)
+                    reason = row.find('a', class_="tag tag--reason").text
+                    reason_.append(reason)
+                    location2 = row.find('div', class_="recall-teaser__states").text
+                    location_.append(location2)
+                    date = row.find('div', class_="recall-teaser__date").text.replace('\n','')
+                    date_.append(date)
+                    summary = row.find('div', class_="recall-teaser__summary").text.replace('\n','')
+                    summary_.append(summary)
+                    impacted_products = row.find('div', class_="recall-teaser__products").text
+                    impacted_products_.append(impacted_products)
+                    link = row.find("a", href=re.compile("^(/recalls-alerts/)((?!:).)*$"))
+                    link_.append(link)
+        page = page + 1
+
+    links=[]
+    for link in link_:
+        item = f"https://www.fsis.usda.gov{link.attrs['href']}"
+        links.append(item)            
+    return view_status, location2, teaser_title, reason, date, summary, impacted_products, item
 
 print(viewUSDA())
