7b17dad923be3c3c764adef507e28c17a51559da
SpaceBarTender <adumas97@gmail.com>
2024-02-04 22:04:43 -0600

    Add connection to pgsql server, initialize cursor variable, comment common cursor and connection methods

--- web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
+++ web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
@@ -1,3 +1,12 @@
 from usdarecalls1nf import dfUSDA
+import psycopg2
 
-print(dfUSDA)
+conn = psycopg2.connect(host="localhost", dbname="postgres", user="sbt",
+                         password='41998', port=5432)
+
+cur = conn.cursor()
+
+# conn.commit()
+
+# cur.close()
+# conn.close()
