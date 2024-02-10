158d97242feaef95828a72b73507d93f8d781740
SpaceBarTender <adumas97@gmail.com>
2024-02-10 06:50:51 -0600

    Create hierarhcy dictionary and turn to dataframe

--- work-related/recursive-query.py
+++ work-related/recursive-query.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+
+data = {
+        'id': [1,2,3,4,5],
+        'Name': ['grandfather','father','child1','child2', 'someone else'],
+        'parent_id': [0, 1,2,2,3]
+    }
+df = pd.DataFrame.from_dict(data)
