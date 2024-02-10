f65aa718f0fd9a62b71a65da26ba145934ff146e
SpaceBarTender <adumas97@gmail.com>
2024-02-10 06:53:29 -0600

    Create 'level' col to be filled by recursive func, add comments

--- work-related/recursive-query.py
+++ work-related/recursive-query.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import numpy as np
 
+# Create example hierarchy dict
 data = {
         'id': [1,2,3,4,5],
         'Name': ['grandfather','father','child1','child2', 'someone else'],
         'parent_id': [0, 1,2,2,3]
     }
+
+
 df = pd.DataFrame.from_dict(data)
+
+# Make sure ids are ints
 df = df.replace(np.nan,'',regex=True)
 df['id'] = df['id'].astype(int)
 df['parent_id'] = df['parent_id'].astype(int)
+
+# Will use recursive function to fill level column
+df['level'] = pd.Series()
