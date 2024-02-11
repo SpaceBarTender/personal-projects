4bc7e28d7f234e1bb3066f13b3389b88bd32a101
SpaceBarTender <adumas97@gmail.com>
2024-02-11 07:41:43 -0600

    chage sql col names to snake_case

--- web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
+++ web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
@@ -11,15 +11,15 @@ cur = conn.cursor()
 cur.execute("""DROP TABLE IF EXISTS recalls1nf""")
 cur.execute("""CREATE TABLE IF NOT EXISTS recalls1nf (
             id SERIAL PRIMARY KEY,
-            RecallStatus VARCHAR(10),
-            Title VARCHAR(100),
-            Reason VARCHAR(100),
-            Summary VARCHAR(1000),
-            Link VARCHAR(200),
-            StartDate DATE,
-            DateStatus VARCHAR(10),
-            ImpactedProducts VARCHAR(1000),
-            Location VARCHAR(100)
+            recall_status VARCHAR(10),
+            title VARCHAR(100),
+            teason VARCHAR(100),
+            tummary VARCHAR(1000),
+            tink VARCHAR(200),
+            start_date DATE,
+            date_status VARCHAR(10),
+            impacted_products VARCHAR(1000),
+            location VARCHAR(100)
 );
 """)
 
@@ -27,11 +27,11 @@ cur.close()
 conn.close()
 
 #create engine dialect+driver://username:password@server/database
-# engine = create_engine('postgresql+psycopg2://postgres:41998@localhost/postgres')
-# print(engine)
+engine = create_engine('postgresql+psycopg2://postgres:41998@localhost/postgres')
+print(engine)
 
-# #df.to_sql methods: append, fail, replace
-# dfUSDA.to_sql('recalls1nf', engine, if_exists='append', index=False)
+#df.to_sql methods: append, fail, replace
+dfUSDA.to_sql('recalls1nf', engine, if_exists='append', index=False)
    
 # conn.commit()
 
