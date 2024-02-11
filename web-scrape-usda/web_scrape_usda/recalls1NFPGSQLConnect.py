ae3f196e2c8fbafad15a72e5a8d49bc347193181
SpaceBarTender <adumas97@gmail.com>
2024-02-11 07:51:14 -0600

    Successfully append dfUSDA to pgsql database

--- web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
+++ web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
@@ -11,18 +11,18 @@ cur = conn.cursor()
 cur.execute("""DROP TABLE IF EXISTS recalls1nf""")
 cur.execute("""CREATE TABLE IF NOT EXISTS recalls1nf (
             id SERIAL PRIMARY KEY,
-            recall_status VARCHAR(10),
-            title VARCHAR(100),
-            teason VARCHAR(100),
-            tummary VARCHAR(1000),
-            tink VARCHAR(200),
+            recall_status VARCHAR,
+            title VARCHAR,
+            reason VARCHAR,
+            summary VARCHAR,
+            link VARCHAR,
             start_date DATE,
-            date_status VARCHAR(10),
-            impacted_products VARCHAR(1000),
-            location VARCHAR(100)
+            date_status VARCHAR,
+            impacted_products VARCHAR,
+            location VARCHAR
 );
 """)
-
+conn.commit()
 cur.close()
 conn.close()
 
@@ -33,7 +33,7 @@ print(engine)
 #df.to_sql methods: append, fail, replace
 dfUSDA.to_sql('recalls1nf', engine, if_exists='append', index=False)
    
-# conn.commit()
+
 
 # # cur.close()
 # # conn.close()
