24ffa95a6c27bfe8d1938cb07da3ee680bc84ad6
SpaceBarTender <adumas97@gmail.com>
2024-02-11 07:23:01 -0600

    Change col names in sqlconnection to df desired names

--- web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
+++ web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
@@ -2,35 +2,34 @@ from usdarecalls1nf import dfUSDA
 import psycopg2
 from sqlalchemy import create_engine
 from psycopg2.extras import execute_values
+import pandas as pd
 
-#create engine dialect+driver://username:password@server/database
-engine = create_engine('postgresql+psycopg2://postgres:41998@localhost/postgres')
-print(engine)
+conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                         password='41998', port=5432)
 
-# conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
-#                          password='41998', port=5432)
+cur = conn.cursor()
+cur.execute("""DROP TABLE IF EXISTS recalls1nf""")
+cur.execute("""CREATE TABLE IF NOT EXISTS recalls1nf (
+            id SERIAL PRIMARY KEY,
+            Status VARCHAR (10),
+            StartDate DATE,
+            DateStatus VARCHAR(10),
+            RecallStatus VARCHAR(10),
+            Title VARCHAR(100),
+            Reason VARCHAR(100),
+            Summary VARCHAR(1000),
+            Link VARCHAR(200),
+            ImpactedProducts VARCHAR(1000),
+            Location VARCHAR(100)
+);
+""")
 
-# cur = conn.cursor()
-# cur.execute("""DROP TABLE IF EXISTS recalls1nf""")
-# cur.execute("""CREATE TABLE IF NOT EXISTS recalls1nf (
-#             id SERIAL PRIMARY KEY,
-#             status VARCHAR (10),
-#             start_date DATE,
-#             date_status VARCHAR(10),
-#             recall_status VARCHAR(10),
-#             title VARCHAR(100),
-#             reason VARCHAR(100),
-#             summary VARCHAR(1000),
-#             link VARCHAR(200),
-#             impacted_products VARCHAR(1000),
-#             location VARCHAR(100)
-# );
-# """)
+# #create engine dialect+driver://username:password@server/database
+# engine = create_engine('postgresql+psycopg2://postgres:41998@localhost/postgres')
+# print(engine)
 
-# for row in dfUSDA.iterrows():
-#     cur.execute("""INSERT INTO recalls1nf (Satus, Title, Reason, Summary, Link, StartDate, DateStatus, Impacted_Products, Location)
-#                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""", row)
-
+# #df.to_sql methods: append, fail, replace
+# dfUSDA.to_sql('recalls1nf', engine, if_exists='append', index=False)
    
 # conn.commit()
 
