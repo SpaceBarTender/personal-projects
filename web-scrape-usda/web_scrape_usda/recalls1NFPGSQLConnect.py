bdd6385ebef19bd48a7c4fc2e7148f2e7699dd29
SpaceBarTender <adumas97@gmail.com>
2024-02-11 06:42:14 -0600

    Drop previous table, alter create table expression to include split date cols

--- web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
+++ web-scrape-usda/web_scrape_usda/recalls1NFPGSQLConnect.py
@@ -5,10 +5,11 @@ conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
                          password='41998', port=5432)
 
 cur = conn.cursor()
-
+cur.execute("""DROP TABLE IF EXISTS recalls1nf""")
 cur.execute("""CREATE TABLE IF NOT EXISTS recalls1nf (
             id INT PRIMARY KEY,
-            date VARCHAR(30),
+            start_date DATE,
+            date_States VARCHAR(10),
             status VARCHAR(10),
             title VARCHAR(100),
             reason VARCHAR(100),
