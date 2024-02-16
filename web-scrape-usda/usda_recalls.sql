8c07ed31a12c33ac77a9fed43572d82805a82871
SpaceBarTender <adumas97@gmail.com>
2024-02-15 21:48:54 -0600

    Add comment stating beginning of transformation to 2nd nf

--- web-scrape-usda/usda_recalls.sql
+++ web-scrape-usda/usda_recalls.sql
@@ -4,5 +4,5 @@ DROP CONSTRAINT recalls1nf_pkey;
 ALTER TABLE recalls1nf
 ADD PRIMARY KEY (title, impacted_products, location); -- Official First Normal Form
 
-ALTER TABLE recalls1nf
+ALTER TABLE recalls1nf -- Begin setting to Second Normal Form
 DROP COLUMN id;
