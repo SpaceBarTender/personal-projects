4cc8965ceea89c9cb50c7a847ed9f559b01dd0be
SpaceBarTender <adumas97@gmail.com>
2024-02-15 22:30:07 -0600

    Add primary composite key, comment state table as officially in 1st nf

--- web-scrape-usda/usda_recalls.sql
+++ web-scrape-usda/usda_recalls.sql
@@ -1,4 +1,6 @@
 UPDATE recalls1nf
 SET impacted_products = 'None given'
 WHERE impacted_products IS NULL;
-       
+
+ALTER TABLE recalls1nf
+ADD PRIMARY KEY (recall_id, impacted_products, location); -- Officially set to First Normal Form
