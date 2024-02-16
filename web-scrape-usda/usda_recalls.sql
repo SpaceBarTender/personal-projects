0655924f70a4e9d051752e95ffaf107e1a44182f
SpaceBarTender <adumas97@gmail.com>
2024-02-15 21:48:10 -0600

    Add comment to state official 1st normal form

--- web-scrape-usda/usda_recalls.sql
+++ web-scrape-usda/usda_recalls.sql
@@ -2,7 +2,7 @@ ALTER TABLE recalls1nf
 DROP CONSTRAINT recalls1nf_pkey;
 
 ALTER TABLE recalls1nf
-ADD PRIMARY KEY (title, impacted_products, location);
+ADD PRIMARY KEY (title, impacted_products, location); -- Official First Normal Form
 
 ALTER TABLE recalls1nf
 DROP COLUMN id;
