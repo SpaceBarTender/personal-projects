8bb0ee69efd88be955338267fd692580cb57d3f6
SpaceBarTender <adumas97@gmail.com>
2024-02-16 07:05:40 -0600

    Create locations table, fill with distinct location values from recalls raw

--- web-scrape-usda/pgsql_files/recallsraw.sql
+++ web-scrape-usda/pgsql_files/recallsraw.sql
@@ -4,5 +4,13 @@ WHERE impacted_product IS NULL;
 
 SELECT * FROM recallsraw;
 
+SELECT * FROM recallsraw;
+
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (location_id SERIAL, location VARCHAR, PRIMARY KEY(location));
+SELECT DISTINCT location FROM recallsraw;
+INSERT INTO locations (location) SELECT DISTINCT location from recallsraw;
+
+
 -- ALTER TABLE recalls1nf
 -- ADD PRIMARY KEY (recall_id, impacted_products, location); -- Officially set to First Normal Form
